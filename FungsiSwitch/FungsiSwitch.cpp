bb84ead6040c4bdd037f8e04d0b785c314331c23
ibnatysalsabila <salsajpr01@gmail.com>
2024-03-26 17:50:29 +0700

    menu pilihan

--- FungsiSwitch/FungsiSwitch.cpp
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -31,5 +31,23 @@ int pembagian(int a, int b)
     return a / b;
 }
 
+int main()
+
+int pilihan;
+do
+{
+    cout << "Kalkulator Sederhana" << endl;
+    cout << "====================" << endl;
+    cout << "1. Penjumlahan" << endl;
+    cout << "2. Pengurangan" << endl;
+    cout << "3. Perkalian" << endl;
+    cout << "4. Pembagian" << endl;
+    cout << "5. Keluar" << endl;
+    cout << "====================" << endl;
+    cout << " Masukan menu pilihan : ";
+    cin >> pilihan;
+}
+
+
 
 
